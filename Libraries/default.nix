e9f566ce0f2e8a65a9a9a0dd4a965011d24df7d5
hangocdinhnew <dinhndrwnew@gmail.com>
2025-09-07 20:21:23 +0700

    Flake.

---
+++ Libraries/default.nix
@@ -0,0 +1,27 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+with pkgs;
+
+let
+    buildDeps = with pkgs; [
+        (
+          rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+            extensions = [ "cargo", "clippy", "rust-docs", "rust-std", "rustc", "rustfmt", "rust-src" ];
+            targets = [ "x86_64-unknown-none" ];
+          })
+        )
+    ];
+
+    runtimeDeps = with pkgs; [
+    ];
+
+    libraryPath = pkgs.lib.makeLibraryPath runtimeDeps;
+in
+mkShell.override { } {
+    nativeBuildInputs = with pkgs; [
+    ] ++ buildDeps;
+
+    buildInputs = with pkgs; [
+    ] ++ runtimeDeps;
+}
---
+++ flake.nix
@@ -0,0 +1,32 @@
+# From SerenityOS.
+{
+  description = "MyonEngine";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = import ./Libraries { inherit pkgs; };
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
